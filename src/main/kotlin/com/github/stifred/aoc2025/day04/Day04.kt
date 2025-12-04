3335037ea51fae15be6af7a33264c1882a008ff4
Stian Fredrik Aune <s.aune@sportradar.com>
2025-12-04 12:09:21 +0100

    Set<Vector2>

--- src/main/kotlin/com/github/stifred/aoc2025/day04/Day04.kt
+++ src/main/kotlin/com/github/stifred/aoc2025/day04/Day04.kt
@@ -1,35 +1,35 @@
 package com.github.stifred.aoc2025.day04
 
 import com.github.stifred.aoc2025.grids.Direction
-import com.github.stifred.aoc2025.grids.Grid2D
 import com.github.stifred.aoc2025.grids.Grid2D.Companion.asGrid
+import com.github.stifred.aoc2025.grids.Vector2
 import com.github.stifred.aoc2025.solutions.parseInput
 import com.github.stifred.aoc2025.solutions.solve
 
 fun main() {
   val pd = parseInput(day = 4) { it.asPrintingDepartment() }
 
-  solve(part = 1, benchmark = false) { pd.accessibleByForklift.size }
-  solve(part = 2, benchmark = false) { pd.totalAccessibleByForkliftCount }
+  solve(part = 1, benchmark = true) { pd.accessibleByForklift.size }
+  solve(part = 2, benchmark = true) { pd.totalAccessibleByForkliftCount }
 }
 
-val Grid2D<Unit>.accessibleByForklift get() = usedPositions.asSequence().filter { pos ->
+val Set<Vector2>.accessibleByForklift get() = asSequence().filter { pos ->
   Direction.all.asSequence()
     .map { pos.move(it) }
-    .filter { elementAt(it) != null }
+    .filter { it in this }
     .take(4)
     .count() < 4
 }.toSet()
 
-val Grid2D<Unit>.totalAccessibleByForkliftCount: Int get() = sequence {
-  val grid = copy()
+val Set<Vector2>.totalAccessibleByForkliftCount: Int get() = sequence {
+  val grid = toMutableSet()
 
   while (true) {
     val accessible = grid.accessibleByForklift.takeIf { it.isNotEmpty() } ?: break
-    grid.removeAt(accessible)
+    grid -= accessible
 
     yield(accessible.size)
   }
 }.sum()
 
-fun String.asPrintingDepartment() = asGrid(mapOf('@' to Unit))
+fun String.asPrintingDepartment() = asGrid(mapOf('@' to true)).usedPositions
